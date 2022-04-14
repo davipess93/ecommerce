3c34d8c976d12cd8c3cda7a989eec53295b1883a
Davi Pessoa <davialvespessoa@gmail.com>
2022-04-13 23:39:57 -0300

    Configurado o template do admin

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
-use Hcode\Page;
 use Slim\Slim;
+use Hcode\Page;
+use Hcode\PageAdmin;
 
 require_once("vendor/autoload.php");
 
@@ -12,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() {
     
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
