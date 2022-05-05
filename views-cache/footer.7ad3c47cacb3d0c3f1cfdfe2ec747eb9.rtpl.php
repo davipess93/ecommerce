bfb952678399d3da45257e2095dd25b5433ba915
Davi Pessoa <davialvespessoa@gmail.com>
2022-05-04 23:16:32 -0300

    Criada toda a lógica para recuperar senha do usuário.

--- views-cache/footer.7ad3c47cacb3d0c3f1cfdfe2ec747eb9.rtpl.php
+++ views-cache/footer.7ad3c47cacb3d0c3f1cfdfe2ec747eb9.rtpl.php
@@ -96,12 +96,6 @@
 <!-- AdminLTE App -->
 <script src="/res/admin/dist/js/app.min.js"></script>
 
-<script>
-  function confirmDelete() {
-    return confirm("Deseja realmente excluir este registro?")
-  }
-</script>
-
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
    Both of these plugins are recommended to enhance the
    user experience. Slimscroll is required when using the
