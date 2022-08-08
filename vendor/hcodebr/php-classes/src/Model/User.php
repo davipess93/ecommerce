857f49057e63fbbe793aa7e97d68794a4545a06b
Davi Pessoa <davialvespessoa@gmail.com>
2022-08-07 22:50:44 -0300

    Pagina de edição do perfil

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -313,6 +313,28 @@
 
     }
 
+    public static function setSuccess($msg) {
+      
+      $_SESSION[User::SUCCESS] = $msg;
+      
+    }
+
+    public static function getSuccess() {
+      
+      $msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : '';
+      
+      User::clearSuccess();
+      
+      return $msg;
+      
+    }
+
+    public static function clearSuccess() {
+      
+      $_SESSION[User::SUCCESS] = NULL;
+      
+    }
+
     public static function setErrorRegister($msg) {
 
       $_SESSION[User::ERROR_REGISTER] = $msg;
