7f83c1ae2ede82fb3c579106e851a11b9e84053b
Davi Pessoa <davialvespessoa@gmail.com>
2022-08-07 21:28:26 -0300

    Criado o registro do site

--- site.php
+++ site.php
@@ -160,7 +160,9 @@
     $page = new Page();
 
     $page->setTpl("login", [
-      'error' => User::getError()
+      'error' => User::getError(),
+      'errorRegister' => User::getErrorRegister(),
+      'registerValues' => (isset($_SESSION['registerValues'])) ? $_SESSION['registerValues'] : ['name' => '', 'email' => '', 'phone' => '']
     ]);
     
   });
@@ -190,5 +192,61 @@
     exit;
     
   });
+
+  $app->post("/register", function() {
+
+    $_SESSION["registerValues"] = $_POST;
+
+    if(!isset($_POST["name"]) || $_POST["name"] == "") {
+      
+      User::setErrorRegister("Preencha o seu nome");
+      header("Location: /login");
+      exit;
+
+    }
+
+    if(!isset($_POST["email"]) || $_POST["email"] == "") {
+      
+      User::setErrorRegister("Preencha o seu e-mail");
+      header("Location: /login");
+      exit;
+
+    }
+
+    if(!isset($_POST["password"]) || $_POST["password"] == "") {
+      
+      User::setErrorRegister("Preencha a senha");
+      header("Location: /login");
+      exit;
+
+    }
+
+    if(User::checkLoginExist($_POST["email"])) {
+      
+      User::setErrorRegister("Este endereço de e-mail já está sendo usado por outro usuário");
+      header("Location: /login");
+      exit;
+      
+    }
+
+    $user = new User();
+
+    $user->setData([
+      "inadmin" => 0,
+      "deslogin" => $_POST["email"],
+      "desperson" => $_POST["name"],
+      "despassword" => $_POST["password"],
+      "desemail" => $_POST["email"],
+      "nrphone" => $_POST["phone"]
+    ]);
+
+    $user->save();
+
+    User::login($_POST["email"], $_POST["password"]);
+
+    header("Location: /checkout");
+    exit;
+
+  })
 
 ?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -319,6 +319,34 @@
 
     }
 
+    public static function getErrorRegister() {
+
+      $msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+      User::clearErrorRegister();
+      
+      return $msg;
+
+    }
+
+    public static function clearErrorRegister() {
+
+      $_SESSION[User::ERROR_REGISTER] = NULL;
+
+    }
+
+    public static function checkLoginExist($login) {
+
+      $sql = new Sql();
+
+      $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", array(
+        ":deslogin"=>$login
+      ));
+
+      return (count($results) > 0);
+      
+    }
+
     public static function getPasswordHash($password) {
       return password_hash($password, PASSWORD_DEFAULT, [
         "cost" => 12
